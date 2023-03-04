8c206bb5752684d254ecaf41a74c605df65c9251
nitish@gaga <nitishgoku01@gmail.com>
2023-03-04 12:53:40 +0530

    removing contractAdd

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,4 +2,4 @@ import Lottery from "./Lottery.json";
 
 export const contractABI = Lottery.abi;
 
-export const contractAdd = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
+export const contractAdd = ''
